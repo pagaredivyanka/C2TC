0193a9058b98cf72b8a58bedff0db18efac831ed
Divyanka Pagare <pagaredivyanka@gmail.com>
2024-06-24 11:59:35 +0530

    Arithmetic operation

--- TNSCoreJava/src/com/tnsif/day18_stream_api/userdefinedfunctionalinterface/FunctionalInterfaceDemo.java
+++ TNSCoreJava/src/com/tnsif/day18_stream_api/userdefinedfunctionalinterface/FunctionalInterfaceDemo.java
@@ -10,6 +10,38 @@ public class FunctionalInterfaceDemo {
 		obj.greet("Students");
 		obj = (s) -> System.out.println("Gutern Morgan " + s);
 		obj.greet("Students");
+		
+		System.out.println("______________________________________________");
+		
+		ArithmaticOperations opr;
+		int result;
+		
+		opr = (x,y)-> x + y;
+		result = opr.perform(100, 20);
+		System.out.println("Addition = " + result);
+		
+		opr = (x,y)-> x - y;
+		result = opr.perform(100, 20);
+		System.out.println("Subtraction = " + result);
+		
+		opr = (x,y)-> x * y;
+		result = opr.perform(100, 20);
+		System.out.println("Multiplication = " + result);
+		
+		opr = (x,y)-> x / y;
+		result = opr.perform(100, 20);
+		System.out.println("Division = " + result);
+		
+		opr = (x, y) -> {
+			int p = 1;
+			while (y>0) {
+				p = p * x;
+				y--;
+			}
+			return p;
+		};
+		result = opr.perform(4,3);
+		System.out.println("Power is : " + result);
 	}
 }
 
