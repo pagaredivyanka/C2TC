f03dc7e1a44f2284fb6b0106cf34adac728f31ba
Divyanka Pagare <pagaredivyanka@gmail.com>
2024-06-28 11:07:41 +0530

    JUnit5 (Life Cycle)

--- TNSCoreJava/src/com/tnsif/testcases/testing/JUnit.java
+++ TNSCoreJava/src/com/tnsif/testcases/testing/JUnit.java
@@ -4,32 +4,56 @@
  * advantage -> save money
  * Types -> 1. Black Box 2. White Box 
  
- * JavaUnit -> integrated tool (Java Method,Classes) for unit testing of a java 
- * Types -> JUnit1, JUnit2, JUnit3 
- * features -> create dynamic test cases
- * JUnit5 = JUnit Platform + 
- * provide testing framework (API)
+  JavaUnit -> integrated tool (Java Method,Classes) for unit testing of a java 
+  Types -> JUnit1, JUnit2, JUnit3 
+  features -> create dynamic test cases
+  JUnit5 = JUnit Platform + 
+  provide testing framework (API)
  
   
- * JUnit Jupyter (JUnit5)- new program model extension model for writing tests and extensions in JUnit5
- * JUnit Vintage (JUnit 3 or 4) - 
- * open source and free library
- * It is not automated testing tool 
- * test Java application 
- * This are Java classes which contain 1 or more unit test methods which test the other classes of application
+  JUnit Jupyter (JUnit5)- new program model extension model for writing tests and extensions in JUnit5
+  JUnit Vintage (JUnit 3 or 4) - 
+  open source and free library
+  It is not automated testing tool 
+  test Java application 
+  This are Java classes which contain 1 or more unit test methods which test the other classes of application
  
  
- * JUnit Annotations
- * @Test -> Test Case
- * @Before -> check something before execution of other test case/ execute some statement or method before each test case  
- * example result is pass before next year exam form 
- * 
- * @After -> check something after execution of other test case
- * @Ignore
- * 
+  JUnit Annotations
+  @Test -> Test Case
+  @Before -> check something before execution of other test case/ execute some statement or method before each test case  
+  example result is pass before next year exam form 
+  
+  @After -> check something after execution of other test case
+  @Ignore
+  
+ 
+ JUnit Engine -> use to run test cases
+ 
+
+ In JUnit 5 test life cycle is driven by 4 primary annotation 
+ @BeforeAll -> when we annotate method with @BeforeAll then it executed before all tests in the current test class
+ @BeforeEach -> annotation method should be executed before each test in the current test class
+ 	ex. variable re-initialization
+ @AfterAll -> when we annotate method with @AfterAll then it executed after all tests in the current test class
+ @AfterEach -> annotation method should be executed after each test in the current test class
  
  
  
- * JUnit Engine -> use to run test cases
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  
  */
