9519a8a5431e81b2648d288acc892b9b2e6d8f68
Divyanka Pagare <pagaredivyanka@gmail.com>
2024-07-20 22:35:59 +0530

    maven project

--- SampleMavenProject/src/main/java/sample/demo/Department.java
+++ SampleMavenProject/src/main/java/sample/demo/Department.java
@@ -1,5 +1,42 @@
 package sample.demo;
 
-public class Department {
+import org.springframework.beans.factory.annotation.Autowired;
 
+public class Department {
+	
+	private int deptNo;
+	private String deptName;
+	
+	@Autowired
+	public Department() {
+		super();
+	}
+
+	public Department(int deptNo, String deptName) {
+		super();
+		this.deptNo = deptNo;
+		this.deptName = deptName;
+	}
+
+	public int getDeptNo() {
+		return deptNo;
+	}
+
+	public void setDeptNo(int deptNo) {
+		this.deptNo = deptNo;
+	}
+
+	public String getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [deptNo=" + deptNo + ", deptName=" + deptName + "]";
+	}
+	
 }
